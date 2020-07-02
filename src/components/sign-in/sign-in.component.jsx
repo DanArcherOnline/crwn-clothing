e9a5b176116c258f0da0edeefaecc77cba0165c9
DanArcherOnline <danarcheronline@gmail.com>
2020-07-02 13:56:00 +0900

    Feat: Add ability to sign in with email & password

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { signInWithGoogle, auth } from '../../firebase/firebase.utils';
 
 
 class SignIn extends React.Component {
@@ -14,12 +14,20 @@ class SignIn extends React.Component {
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
-        this.setState({
-            email: '',
-            password: ''
-        });
+        const { email, password } = this.state;
+
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({
+                email: '',
+                password: ''
+            });
+        } catch (error) {
+            console.error(error);
+        }
+
     }
 
     handleChange = event => {
@@ -50,7 +58,7 @@ class SignIn extends React.Component {
                         required />
 
                     <div className='buttons'>
-                        <CustomButton type='submit'>Submit Form</CustomButton>
+                        <CustomButton type='submit'>SIGN IN</CustomButton>
                         <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
                             Sign In With Google
                     </CustomButton>
