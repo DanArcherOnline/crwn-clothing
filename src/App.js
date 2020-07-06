bc6668cde16a975bc46b7782383814ae83de224d
DanArcherOnline <danarcheronline@gmail.com>
2020-07-06 22:52:39 +0900

    Feat: Group cart items to store item quantity
    - update quantity when adding multiple items of the same item
    - update shop.data.js so that all cart items have unqiue id's

--- src/App.js
+++ src/App.js
@@ -46,6 +46,7 @@ class App extends React.Component {
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
           <Route
+            exact
             path='/signin'
             render={() =>
               this.props.currentUser ? (
